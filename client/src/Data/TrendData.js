15a7ff2af84484c9027555573a6b6a5b8caa354f
VINAY <vinayselwal@gmail.com>
2023-10-15 23:08:13 +0530

    files added

---
+++ client/src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData= [
+  {
+    name: "Minions",
+    shares: 97,
+  },
+  {
+    name: "Avengers",
+    shares: 80.5,
+  },
+  {
+    name: "Connect",
+    shares: 75.5,
+  },
+  {
+    name: "Ymca",
+    shares: 72,
+  },
+  {
+    name: "Computer Science",
+    shares: 71.9,
+  },
+  {
+    name: "Vasundhara",
+    shares: 20,
+  },
+];
