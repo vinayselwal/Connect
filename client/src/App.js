63fd3374c506a7bb6c3c1a93fcad7cbbc428209b
VINAY <vinayselwal@gmail.com>
2024-07-18 00:10:18 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
       className="App"
       style={{
         height:
-          window.location.href === "http://localhost:3000/chat"
+          window.location.href === "https://connect-two.vercel.app/chat"
             ? "calc(100vh - 2rem)"
             : "auto",
       }}
